4f86d048edb1c3b6cf8a39b27468feffcf1f4abb
JoelZhub <joelalbertobenitezvarela28@gmail.com>
2025-08-24 14:51:50 -0400

    #terminacion del ejercicio

--- ejercicio24082025.cs
+++ ejercicio24082025.cs
@@ -1,7 +1,7 @@
 // Crear un programa que solicite una lista de números al usuario y 
 //luego muestre los números ordenados de menor a mayor sin usar métodos de ordenamiento incorporados.
 int num;
-while (!int.tryparse(Console.ReadLine(), out num))
+while (!int.TryParse(Console.ReadLine(), out num))
 {
     System.Console.WriteLine("Ingrese un numero valido");
     Console.ReadLine();
@@ -9,35 +9,38 @@ while (!int.tryparse(Console.ReadLine(), out num))
 
 int[] arrays = new int[num];
 int numIngresado;
-for (int i = 0; i < arrays.lenght; i++)
+for (int i = 0; i < arrays.Lenght; i++)
 {
 
     System.Console.WriteLine($"Ingrese el numero {i + 1} ");
-    while (!int.tryparse(Console.ReadLine(), out numIngresado))
+    while (!int.TryParse(Console.ReadLine(), out numIngresado))
     {
         System.Console.WriteLine("Ingrese un numero valido");
         Console.ReadLine();
     }
 
     arrays[i] = numIngresado;
+    Console.Clear();
 
-    for (int j = 1; j < arrays.lenght - 1; j++)
-    {
 
-        int temp = 0;
 
-        if (arrays[i] > arrays[j])
-        {
-            arrays[i] = temp;
-            arrays[i] = arrays[j];
-            arrays[j] = temp;
-        }
-     
-    }
 }
+
+for (int j = 0; j < arrays.Length-1; j++)
+{
+ int temp;
+  
+    if (arrays[j] > arrays[j + 1])
+    {
+        temp = arrays[j];
+        arrays[j] = arrays[j + 1];
+        arrays[j + 1] = temp;
+    }
+
+    }
 
 foreach (var item in arrays)
 {
-    
+
     System.Console.WriteLine($"\n{item}");
 }
