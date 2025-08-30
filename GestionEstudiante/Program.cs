68e8e6c0dde97a3181df0fbb8843db486bc6ed1b
JoelZhub <joelalbertobenitezvarela28@gmail.com>
2025-08-30 09:15:24 -0400

    #terminacion del ejercicio

--- GestionEstudiante/Program.cs
+++ GestionEstudiante/Program.cs
@@ -19,7 +19,7 @@ class Estudiantes
 
     public static List<Estudiantes> listaEstudiantes = new List<Estudiantes>();
 
-    public static List<double> Calificaciones { get; set; }
+    public  List<double> Calificaciones { get; set; }
 
 
     public Estudiantes(string nombre)
@@ -40,14 +40,14 @@ class Estudiantes
         while (!int.TryParse(Console.ReadLine(), out opt) || opt <= 0 || opt > 5)
         {
             Console.Clear();
-            Console.Write("Ingrese una accion valida");
+            Console.Write("Ingrese una accion valida:  ");
+
             Console.ReadLine();
 
         }
 
         switch (opt)
         {
-
 
             case 1:
 
@@ -58,25 +58,23 @@ class Estudiantes
                 while (!int.TryParse(Console.ReadLine(), out numeroRegistros) || numeroRegistros <= 0)
                 {
                     Console.Clear();
-                    Console.Write("Error: Ingrese un numero valido");
-                    Console.ReadLine();
+                    Console.Write("Error: Ingrese un numero valido: ");
+
                 }
 
                 CrearEstudiantes(numeroRegistros);
                 Console.Clear();
-                Console.WriteLine("Estudiantes agregados con exito");
-             Console.WriteLine(listaEstudiantes[1].Nombre);
+                Console.WriteLine("Estudiantes agregados con exito ");
 
-                Thread.Sleep(3000);
+                Thread.Sleep(1000);
                 Console.Clear();
                 Menu();
-
                 break;
 
             case 2:
                 if (listaEstudiantes.Count == 0)
                 {
-                    
+
                     Console.Clear();
                     Console.WriteLine("Error: No existen estudiantes ");
                     Console.WriteLine("Desea agregar estudiantes(Si/No)");
@@ -84,93 +82,150 @@ class Estudiantes
 
                     if (optIngresada != "si") Menu();
 
-                        Console.WriteLine("Ingrese el numero de estudiantes a registrar");
-                        int num;
-                        while (!int.TryParse(Console.ReadLine(), out num) || num <= 0)
-                        {
-                            Console.WriteLine("Ingrese un numero valido");
-                            Console.ReadLine();
-                        }
+                    Console.WriteLine("Ingrese el numero de estudiantes a registrar");
+                    int num;
+                    while (!int.TryParse(Console.ReadLine(), out num) || num <= 0)
+                    {
+                        Console.WriteLine("Ingrese un numero valido: ");
+                        Console.ReadLine();
+                    }
 
-                        CrearEstudiantes(num);
+                    CrearEstudiantes(num);
 
-                        Console.Clear();
-                        Menu();
-                    
+                    Console.Clear();
+                    Menu();
+
 
                 }
 
+                Console.Clear();
+
                 Console.WriteLine("Cuantas calificaciones desea agregar por estudiante? ");
-                int numCalificaciones = 0;
+                int numCalificaciones;
+
+                while (!int.TryParse(Console.ReadLine(), out numCalificaciones))
+                {
+
+                    Console.Write("Error: Ingrese un numero valido: ");
+                }
 
                 for (int i = 0; i < listaEstudiantes.Count; i++)
                 {
 
-                    for (int j = 0; j < numCalificaciones ; i++)
+                    for (int j = 0; j < numCalificaciones; j++)
                     {
-                        Console.WriteLine($"Ingrese la calificacion {j + 1} \n Para el estudiante {listaEstudiantes[i].Nombre}");
+                        Console.Clear();
+                        Console.WriteLine($"Para el estudiante {listaEstudiantes[i].Nombre}");
+                        Console.WriteLine($"Ingrese la calificacion {j + 1}");
                         double calificacion;
 
                         while (!double.TryParse(Console.ReadLine(), out calificacion) || calificacion <= 0)
                         {
-                            Console.WriteLine("Ingrese una calificacion valida");
-                            Console.ReadLine();
+                            Console.Clear();
+                            Thread.Sleep(1000);
+                            Console.Write("Error: Ingrese una calificacion valida: ");
+
                         }
 
-                        AgregarCalificaciones(calificacion);
+                        listaEstudiantes[i].AgregarCalificaciones(calificacion);
                     }
 
-                 }
+                }
+                Console.Clear();
+                Menu();
 
                 break;
 
             case 3:
-                if (Calificaciones.Count == 0 || listaEstudiantes.Count == 0)
+
+                Console.Clear();
+
+                if (listaEstudiantes.Count <= 0)
                 {
-                    Console.WriteLine("No hay califaciones  o estudiantes agregadoss ");
+                    Console.WriteLine("Error: No existen estudiantes ");
+                    Thread.Sleep(1500);
                     Console.Clear();
                     Menu();
 
                 }
-            Console.Clear();
+                Console.Clear();
 
-              Console.WriteLine("Ingrese el indice de la calificacion a eliminar");
+                Console.Write("Ingrese el indice del estudiante sobre el que desea operar: ");
 
-                int indice;
+                int indiceEstudiante;
 
-                while (!int.TryParse(Console.ReadLine(), out indice))
+                while (!int.TryParse(Console.ReadLine(), out indiceEstudiante) || indiceEstudiante < 0 || indiceEstudiante > listaEstudiantes.Count)
                 {
-                    Console.WriteLine("Ingrese un indice valido");
-                    Console.ReadLine();
+
+                    Console.Clear();
+                    Console.Write($"Error: Ingrese un indice valido ");
+                    Console.Clear();
+
                 }
 
-                if (EliminarCalificacion(indice))
+                int indiceCalificacion;
+
+
+                Console.WriteLine($"Ingrese el indice de la calificacion a eliminar para el estudiante: {listaEstudiantes[indiceEstudiante].MostrarNombre()} ");
+
+                while (!int.TryParse(Console.ReadLine(), out indiceCalificacion) || indiceCalificacion < 0 || indiceCalificacion > listaEstudiantes[indiceEstudiante].Calificaciones.Count)
+                {
+                    Console.Clear();
+                    Console.Write("Error: Ingrese un indice validio ");
+                    Console.Clear();
+
+                }
+
+                Console.WriteLine($"Datos a eliminar del estudiante: {listaEstudiantes[indiceEstudiante].MostrarNombre()} \n Calificacion: {listaEstudiantes[indiceEstudiante].Calificaciones[indiceCalificacion]}");
+
+                if (listaEstudiantes[indiceEstudiante].EliminarCalificacion(indiceCalificacion))
                 {
                     Console.WriteLine("Calificacion eliminada con exito");
+                    Thread.Sleep(4000);
                     Console.Clear();
                     Menu();
                 }
                 else
                 {
                     Console.WriteLine("No se pudo eliminar la calificacion");
+                    Thread.Sleep(4000);
                     Console.Clear();
                     Menu();
                 }
-
                 break;
 
             case 4:
 
-                if (listaEstudiantes.Count <= 0 || Calificaciones.Count <= 0)
+                if (listaEstudiantes.Count <= 0)
                 {
-                    Console.WriteLine("No hay estudiantes o califaciones para mostrar");
+                    Console.WriteLine("No hay estudiantes para mostrar");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("Regresando...");
+                    Thread.Sleep(1000);
                     Console.Clear();
                     Menu();
                 }
 
-             
+                Console.Clear();
+
+                for (int i = 0; i < listaEstudiantes.Count; i++)
+                {
 
 
+                    Console.WriteLine($"{listaEstudiantes[i].MostrarInforamacion()} \n ");
+
+                }
+
+                Thread.Sleep(5000);
+                Console.Clear();
+                Menu();
+                break;
+
+            case 5:
+
+                Console.Clear();
+                Console.WriteLine("Saliendo del programa...");
 
                 break;
 
@@ -183,21 +238,27 @@ class Estudiantes
 
         }
 
-
-
     }
+    
 
-    public  void MostrarInforamacion()
+
+    public string MostrarInforamacion()
     {
-        Console.WriteLine($"Estudiante: {Nombre}  \n Promedio: {Promedio:f2} \n Calificaciones: {string.Join(", ", Calificaciones)}");
+        return $"Estudiante: {Nombre} \n Promedio: {Promedio} \n Calificaciones: {string.Join(", ", Calificaciones)}";
     }
 
-    public static void AgregarCalificaciones(double calificaciones)
+    public string MostrarNombre()
+    {
+        return Nombre;
+        
+    }
+
+    public void AgregarCalificaciones(double calificaciones)
     {
         Calificaciones.Add(calificaciones);
 
     }
-    public static double Promedio
+    public  double Promedio
     {
         get
         {
@@ -205,10 +266,12 @@ class Estudiantes
             {
                 return 0;
             }
+          
+          
             return Calificaciones.Average();
         }
     }
-    public static bool EliminarCalificacion(int indice)
+    public  bool EliminarCalificacion(int indice)
     {
         if (indice >= 0 && indice <= Calificaciones.Count)
         {
@@ -218,22 +281,45 @@ class Estudiantes
         return false;
     }
 
+
+    public static string Nombrevalido(string nombre)
+    {
+
+
+        while (string.IsNullOrEmpty(nombre) || nombre.Any(char.IsDigit) || listaEstudiantes.Any(v => v.Nombre.ToLower() == nombre.ToLower()))
+        {
+            Console.Clear();
+            if (string.IsNullOrEmpty(nombre) || nombre.Any(char.IsDigit))
+            {
+                Console.Write("Error: Ingrese un nombre valido para {0} ", nombre);
+                nombre = Console.ReadLine()!;
+            }
+            else
+            {
+
+                Console.Write($"Error: El nombre ya existe ");
+            }
+
+            nombre = Console.ReadLine()!;
+
+        }
+
+        return nombre.ToLower();
+}
     public static void CrearEstudiantes(int numeroRegistros)
     {
 
 
         for (int i = 0; i < numeroRegistros; i++)
         {
-            Console.Write("Ingrese el nombre del estudiante: ", i + 1);
-            string? nombre = Console.ReadLine();
 
-            while (string.IsNullOrEmpty(nombre) || nombre.Any(char.IsDigit))
-            {
-                Console.Write("Ingrese un nombre valido");
-                nombre = Console.ReadLine();
-            }
+            Console.Clear();
+            Console.Write($"Ingrese el nombre del estudiante {i + 1}: ");
+            string? nombre = Console.ReadLine()!.ToLower();
 
-            listaEstudiantes.Add(new Estudiantes(nombre));
+            nombre = Nombrevalido(nombre!);
+            
+            listaEstudiantes.Add(new Estudiantes(nombre!.ToLower()));
 
         }
 
