45484bfbabc5ed00f533251fe955512b1e31ff6e
JoelZhub <joelalbertobenitezvarela28@gmail.com>
2025-09-13 10:42:20 -0400

    #creacion de login, validaciones con script

---
+++ JS/practicas/practica02Js/script.js
@@ -0,0 +1,58 @@
+const msError = document.getElementById('error-messaje');
+
+const email = document.getElementById('email');
+
+const password = document.getElementById('password');
+
+const btn = document.getElementById('btn-login');
+
+const msEmail = document.getElementById('email-error');
+
+if(email){
+
+
+email.addEventListener("input", () => {
+
+    const value = email.value.trim();   
+    
+    if(!(value.includes("@") && value.includes("."))){
+        msEmail.classList.remove("hidden");
+        email.classList.add("error-border")
+       
+    }else{
+        msEmail.classList.add("hidden");
+        email.classList.remove("error-border")
+    }
+
+});
+
+
+btn.addEventListener("click", () => {
+
+    if(email.value != "joel@gmail.com" && password.value!= "1234"){
+        msError.classList.remove("hidden");
+       setTimeout(() => msError.classList.add("hidden"), 3000);
+
+    }else{
+
+        const userName = email.value.trim();
+
+        localStorage.setItem("usuario", userName);
+
+        window.location.href = "home.html";
+        
+    }
+
+});
+
+}
+
+const welcome = document.getElementById('welcome');
+
+if(welcome){
+const userName = localStorage.getItem("usuario");
+welcome.innerText = userName ? `welcome ${userName}` : "welcome";   
+localStorage.clear();
+}
+
+
