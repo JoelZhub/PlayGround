fd04058d90a5595b072b60488b63460a6e150546
JoelZhub <joelalbertobenitezvarela28@gmail.com>
2025-09-13 11:50:24 -0400

    #ejercicio basicos en js

---
+++ JS/practicas/EjerciciosBasicJs/ejercicio1.js
@@ -0,0 +1,45 @@
+let totalHora = 384000  / 1225
+console.log(` Ejercicio 1: El total de horas de la nave en llegar a la luna es:  ${Math.round(totalHora)}`);
+
+let totalCookies = 7 *8;
+console.log(` Ejercicio 2: El total de cookies es:  ${totalCookies}`);
+
+let totalMinutos =  7 * 60;
+console.log(` Ejercicio 3: El total de minutos es:  ${totalMinutos}`);
+
+let pesoTotal = 1120 + 141;
+console.log(` Ejercicio 4: El total de minutos es:  ${pesoTotal}`);
+
+let venta = 3.8 * 16;
+
+console.log(`Ejercicio 5: El total de venta es:  ${venta}`);
+
+let precioJuego = 50;
+
+let descuento = 50 * 0.2;
+
+let precioFianl = precioJuego - descuento;
+
+console.log(`Ejercicio 6: El precio final del juego es:  ${precioFianl}`);
+
+let radio = 18 / 2;
+
+console.log(`Ejercicio 7: El precio final del juego es:  ${precioFianl}`);
+
+let precioBola = 1.8;
+
+let una = precioBola * 1;
+let dos = precioBola * 2;
+let tres = precioBola * 3;
+
+console.log(`Ejerccio 8: El precio del helado de una bola es: ${una+0.50} El de dos: ${dos+0.50}  el de tres: ${tres+0.50}`);
+
+let totalKilometros  = 120 * 1.6;
+console.log(`Ejercicio 9: El total de total Kilometros  final es:  ${totalKilometros}`);
+
+let yo = 2;
+let pepe = 3;
+let manolo = 6;
+let totalHamburgueza = yo + pepe + manolo;
+let escierto = yo < pepe;
+console.log(`Ejercicio 10: El total de total de hamburguesas  final es:  ${totalHamburgueza} Victor comio menos?: ${escierto}`);
