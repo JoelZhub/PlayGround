a846fc53a5ab10f3265a6099b7ac46b8b2e4490e
JoelZhub <joelalbertobenitezvarela28@gmail.com>
2025-12-29 10:45:35 -0400

    asyn/await

--- JS/practicas/Fetch/script.js
+++ JS/practicas/Fetch/script.js
@@ -25,23 +25,40 @@ import { CreateUserPost } from "./User/CreateUserPost.js";
 
 //guardar  informacion  
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json; charset=UTF-8" 
-    },
-     body: JSON.stringify(new CreateUserPost('de', 'bar', 23))
-})
-.then(res => res.json())
-.then(data => {
-    console.log(data);
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json; charset=UTF-8" 
+//     },
+//      body: JSON.stringify(new CreateUserPost('de', 'bar', 23))
+// })
+// .then(res => res.json())
+// .then(data => {
+//     console.log(data);
 
-}).catch(error => {
-    console.error("Error en la solicitud post " + error);
-});
-
-
+// }).catch(error => {
+//     console.error("Error en la solicitud post " + error);
+// });
 
 
 //asyn /await
 
+// async function listUsers (){
+// try{
+//      let  res = await fetch('https://jsonplaceholder.typicode.com/users/');
+//     let data = await res.json();
+//     data.forEach(element => {
+//             const user = new CreateUser(element);
+//             console.log(user.getDescripcion());
+//         });
+
+// }catch(error){
+//             console.error("Error: " + error);
+//     }
+// }
+
+// listUsers();
+
+
+
+//promesas
