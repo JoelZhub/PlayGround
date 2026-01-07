975da9db4ac26119a9bb0a778827ee3b36073333
JoelZhub <joelalbertobenitezvarela28@gmail.com>
2026-01-07 13:03:43 -0400

    creacion de header component

---
+++ ladingPage2/js/components/header.js
@@ -0,0 +1,30 @@
+export function createHeader(){
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `<header class="grid grid-cols-5 max-w-full min-h-14 
+    text-4xl px-4 py-4
+     bg-white border border-b-sky-950 items-center tracking-widest  text-sky-950 " >
+
+     
+    <div class="flex items-center col-span-3">
+    <i class="bxr bx-core text-5xl "></i> 
+    Lading <b class=" text-sky-700">Page</b>
+    </div>
+
+    <div class="sm:flex  hidden col-span-2 max-w-2xs gap-6 md:gap-8">
+            <div class="flex items-center">
+                    <input class=" border border-gray-400 px-9 rounded-sm w-46 md:w-2xs h-7 text-sm outline-none font-normal " type="text" placeholder="Buscar">
+                    <i class="bx  bx-search absolute text-2xl px-2 "></i> 
+            </div>
+
+            <div class="flex items-center gap-1 md:gap-5" >
+                <i class=" cursor-pointer text-2xl p-1.5 hover:border  hover:rounded-md  bx  bx-bell"></i> 
+
+                <i class=" cursor-pointer text-2xl bx border border-gray-300
+                rounded-md p-1.5 hover:bg-black hover:text-white  bx-night-light"></i> 
+            </div>
+
+    </div>
+    </header>`;
+
+    return wrapper.firstChild;
+}
---
+++ ladingPage2/js/script.js
@@ -0,0 +1,5 @@
+import { createHeader } from "./components/header.js";
+
+const header = document.querySelector('#header');
+
+header.appendChild(createHeader());
